777bfc97ebd7fb794a0d4a04f34ef461b22a3028
Nishan <mr.raiali323@gmail.co>
2023-02-24 16:32:47 +0545

    tuple class

---
+++ tuple.py
@@ -0,0 +1,63 @@
+t=(1,2,3,4,60)
+count=t.count(1)
+print(count)
+s=sum(t)
+print("sum is",s)
+
+
+
+tup1=(2,3,1,4,5)
+print(sorted(tup1))
+
+
+student= {
+    "name":"Nishan",
+    "age":24,
+    "address":"Chitwan"
+}
+
+print(student)
+print(student.get("age"))
+
+student["phone"]=9826279512,
+print(student)
+
+
+a={'x': 1, 'z':3, 'y':2}
+
+print(all(a))
+
+print(any(a))
+
+
+
+y={'a':1, 'c':3, 'd':4, 'b':2}
+print(y.keys())
+
+print(y.values())
+
+print(y.items())
+
+x=y.copy()
+
+print(x)
+a={'e':5}
+y.update(a)
+print(a)
+print(y)
+print(y)
+print(y)
+print(y)
+print(y)
+print(y)
+print(y)
+
+print(y)
+
+
+
+
+
+
+
+print(y)
